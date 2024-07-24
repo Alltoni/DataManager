fce32aaa109aadc1e4a1acf1c7238dd6622b98c2
Alltoni <mwwroblewski2000@gmail.com>
2024-07-24 16:45:10 +0200

    #init w modelu animals napisalem co chce osiagnac

---
+++ Models/Animal.cs
@@ -0,0 +1,37 @@
+﻿
+
+namespace DataManager.Models
+{
+    public class Animal
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Animal(string name)
+        {
+            Name = name;
+        }
+
+        // Chce, aby aplikacja działała tak, że użytkownik podaje nazwe zwierzecia ogólna
+        // papuga po czym jezeli jest to w bazie danych to wypisuje te rzeczy ktore sa ponizej
+        // oraz chcialem jeszcze zeby byl mozliwy dzwiek tego zwierzecia jezeli jest dostepny ale to na razie 
+        // chce osiagnac to, zeby bylo tylko nazwa byla przyjmowana od uzytkownika i dostawal informacje o papugach
+
+        //public string Kingdom { get; set; }
+
+        //public string Phylum { get; set; }
+
+        //public string Class { get; set; }
+        //public string Order { get; set; }
+
+        //public string Family { get; set; }
+
+        //public string Genus { get; set; }
+
+        //public string Species { get; set; }
+
+        //public sound
+
+
+    }
+}
---
+++ Services/IAnimalService.cs
@@ -0,0 +1,16 @@
+﻿using DataManager.Models;
+
+
+namespace DataManager.Services
+{
+    public interface IAnimalService
+    {
+        ICollection<Animal> GetAnimals();
+        Animal GetAnimalById(int id);
+        Animal AddAnimal(Animal animal);
+        Animal UpdateAnimal(Animal animal);
+        void DeleteAnimalById(int id);
+
+        public void ClearAnimals();
+    }
+}
